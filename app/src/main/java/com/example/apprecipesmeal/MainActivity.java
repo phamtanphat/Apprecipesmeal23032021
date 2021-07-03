1dd65020b91de95fde147a94c5c4b8790b35fe3b
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-03 21:27:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/apprecipesmeal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.apprecipesmeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

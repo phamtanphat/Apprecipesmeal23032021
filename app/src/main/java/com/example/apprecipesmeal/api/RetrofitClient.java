a57afcecf7710f4222541b9d75186959d667b0ed
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-06 20:06:49 +0700

    set up client

---
+++ app/src/main/java/com/example/apprecipesmeal/api/RetrofitClient.java
@@ -0,0 +1,48 @@
+package com.example.apprecipesmeal.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import retrofit2.Retrofit;
+import retrofit2.Retrofit.Builder;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static RetrofitClient instance = null;
+    private Retrofit retrofit;
+
+    private RetrofitClient(){
+        retrofit = createRetrofit();
+    }
+
+    public static RetrofitClient getInstance(){
+        if (instance == null){
+            instance = new RetrofitClient();
+        }
+        return instance;
+    }
+
+    private Retrofit createRetrofit(){
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30 , TimeUnit.SECONDS)
+                .writeTimeout(30,TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                .build();
+
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://phatdroid94.000webhostapp.com/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/example/apprecipesmeal/view/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.apprecipesmeal.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.apprecipesmeal.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
